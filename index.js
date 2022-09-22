dc95138d011791b90073fe0c4719ef9f72fcd853
Soren <sorenisme@gmail.com>
2022-09-22 08:46:28 -0400

    commit tree 709e25579e1b46a6bd6bb5016658f26cd0f9bb7c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+main.remove('main')
+
+let newHeader = document.createElement("h1");
+
+newHeader.textContent += "is the champion";
+
+newHeader.setAttribute("id", 'victory')
+
+
+
+
+
+
+
